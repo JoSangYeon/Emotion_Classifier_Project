cb69dfc0bc070c994c0d5770941b32fb7d9e3642
JoSangYeon <whtkddus98@kookmin.ac.kr>
2022-05-27 21:21:12 +0900

    Define Model 1~6

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,8 +14,8 @@ class MyModel_1(nn.Module):
 
         self.bert = AutoModel.from_pretrained(model_path)
 
-        self.fc1 = nn.Linear(768, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(768, 256)
+        self.fc2 = nn.Linear(256, num_classes)
 
         self.drop = nn.Dropout(p=0.2)
         self.act_fn = nn.ReLU()
@@ -23,12 +24,12 @@ class MyModel_1(nn.Module):
         bert_output = self.bert(input_ids, attention_mask)
         cls_vector = bert_output.pooler_output              # (batch, 768)
 
-        x = self.fc1(cls_vector)
-        x = self.act_fn(x)
+        x_embed = self.fc1(cls_vector)
+        x = self.act_fn(x_embed)
         x = self.drop(x)
 
         x = self.fc2(x)
-        return x
+        return x, x_embed
     
 
 class MyModel_2(nn.Module):
@@ -39,8 +40,8 @@ class MyModel_2(nn.Module):
 
         self.bert = AutoModel.from_pretrained(model_path)
 
-        self.fc1 = nn.Linear(768, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(768, 256)
+        self.fc2 = nn.Linear(256, num_classes)
 
         self.drop = nn.Dropout(p=0.2)
         self.act_fn = nn.ReLU()
@@ -55,12 +56,12 @@ class MyModel_2(nn.Module):
         all_embed = bert_output.last_hidden_state        # All Token Embed : (batch, 128, 768)
         mean_pool = self.mean_pooling(all_embed, attention_mask) # (batch, 768)
 
-        x = self.fc1(mean_pool)
-        x = self.act_fn(x)
+        x_embed = self.fc1(mean_pool)
+        x = self.act_fn(x_embed)
         x = self.drop(x)
 
         x = self.fc2(x)
-        return x
+        return x, x_embed
     
 class MyModel_3(nn.Module):
     def __init__(self, 
@@ -87,16 +88,15 @@ class MyModel_3(nn.Module):
         x = self.drop(x)
 
         x = self.fc2(x)
+        x_embed = self.contra(x_embed)
         
         if (c_inputs is not None) and (c_atts is not None):
             c_output = self.bert(c_inputs, c_atts)
             c_embed = c_output.pooler_output         # CLS Token Embed : (batch, 768) 
-            
-            x_embed = self.contra(x_embed)
             c_embed = self.contra(c_embed)
             return x, x_embed, c_embed
         else:
-            return x
+            return x, x_embed
     
     
 class MyModel_4(nn.Module):
@@ -129,24 +129,25 @@ class MyModel_4(nn.Module):
         x = self.drop(x)
 
         x = self.fc2(x)
+        x_embed = self.contra(x_embed)
         
         if (c_inputs is not None) and (c_atts is not None):
             c_output = self.bert(c_inputs, c_atts)
             c_embed = c_output.last_hidden_state         # All Token Embed : (batch, 128, 768)
             c_embed = self.mean_pooling(c_embed, c_atts) # (batch, 768)
-            
-            x_embed = self.contra(x_embed)
             c_embed = self.contra(c_embed)
             return x, x_embed, c_embed
         else:
-            return x
+            return x, x_embed
         
 
 class MyModel_5(nn.Module):
     def __init__(self, 
+                 device='cuda' if torch.cuda.is_available() else 'cpu',
                  model_path="klue/bert-base",
                  num_classes=3):
         super(MyModel_5, self).__init__()
+        self.device=device
 
         self.bert = AutoModel.from_pretrained(model_path)
         
@@ -156,7 +157,6 @@ class MyModel_5(nn.Module):
         
         self.drop = nn.Dropout(p=0.2)
         self.act_fn = nn.ReLU()
-        
 
     def forward(self, x_inputs, x_atts, p_inputs=None, p_atts=None, n_inputs=None, n_atts=None):
         x_output = self.bert(x_inputs, x_atts)
@@ -167,6 +167,7 @@ class MyModel_5(nn.Module):
         x = self.drop(x)
 
         x = self.fc2(x)
+        x_embed = self.toEmbed(x_embed)          # (batch, 768) -> (batch, 256)
         
         if (p_inputs is not None) and (n_inputs is not None):
             p_output = self.bert(p_inputs, p_atts)
@@ -175,12 +176,11 @@ class MyModel_5(nn.Module):
             n_output = self.bert(n_inputs, n_atts)
             n_embed = n_output.pooler_output         # CLS Token Embed : (batch, 768) 
             
-            x_embed = self.toEmbed(x_embed)          # (batch, 768) -> (batch, 256)
             p_embed = self.toEmbed(p_embed)          # (batch, 768) -> (batch, 256)
             n_embed = self.toEmbed(n_embed)          # (batch, 768) -> (batch, 256)
             return x, x_embed, p_embed, n_embed
         else:
-            return x
+            return x, x_embed
     
     
 class MyModel_6(nn.Module):
@@ -214,6 +214,7 @@ class MyModel_6(nn.Module):
         x = self.drop(x)
 
         x = self.fc2(x)
+        x_embed = self.toEmbed(x_embed)          # (batch, 768) -> (batch, 256)
         
         if (p_inputs is not None) and (n_inputs is not None):
             p_output = self.bert(p_inputs, p_atts)
@@ -224,12 +225,11 @@ class MyModel_6(nn.Module):
             n_embed = n_output.last_hidden_state     # All Token Embed : (batch, 128, 768)
             n_embed = self.mean_pooling(n_embed, n_atts) # mean pooling : (batch, 768)
             
-            x_embed = self.toEmbed(x_embed)          # (batch, 768) -> (batch, 256)
             p_embed = self.toEmbed(p_embed)          # (batch, 768) -> (batch, 256)
             n_embed = self.toEmbed(n_embed)          # (batch, 768) -> (batch, 256)
             return x, x_embed, p_embed, n_embed
         else:
-            return x
+            return x, x_embed
 
 
 def get_Model(class_name, model_path):
